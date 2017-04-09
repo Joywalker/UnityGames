122f9f960cdea47854f6b89ca406840920a9c1b3
DESKTOP-03RLU6H\Joywalker <alexandrurazvant525@gmail.com>
2017-04-09 23:06:06 +0300

    Added Moving Starfield

---
+++ LaserDefend/Assets/Sprites/ScrollBK.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBK : MonoBehaviour {
+
+    private float parralax = 2f;
+	void Update () {
+
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material material = mr.material;
+
+        Vector2 offset = material.mainTextureOffset;
+
+        offset.y += Time.deltaTime / 10f;
+        material.mainTextureOffset = offset;
+	}
+}
